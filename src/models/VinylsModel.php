8ce73dd9d7a2c11372ad3795d9c2456cd18bb296
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-12-06 16:38:10 +0100

    added single vinyl get method

--- src/models/VinylsModel.php
+++ src/models/VinylsModel.php
@@ -79,6 +79,64 @@ final class VinylsModel {
         return json_encode($vinyls);
     }
 
-
+    public function getVinylDetails($id) {
+        $details = new stdClass();
+        // query to get vinyls info
+        $vinyl = "SELECT 
+            v.id_vinyl,
+            v.cost,
+            v.rpm,
+            v.inch,
+            v.type,
+            a.id_album,
+            a.title,
+            a.release_date,
+            a.cover_img,
+            ar.name AS artist,
+            FROM 
+            vinyls v
+            JOIN albums a ON v.id_vinyl = a.id_album
+            JOIN inside_album ia ON a.id_album = ia.id_album
+            JOIN artists ar ON ar.id_artist = a.id_artist
+            WHERE v.id_vinyl = ?";
+        // query to get the tracks from a vinyl [needs id_album from previous query]
+        $tracks = "SELECT
+            t.title
+            t.duration
+            FROM
+            albums a
+            JOIN inside_album ia ON ia.id_album = a.id_album
+            JOIN tracks t ON t.id_track = ia.id_track
+            WHERE a.id_album = ?";
+        // prepare statement
+        $stmt = $this->conn->prepare($vinyl);
+        $stmt->bind_params("i", $id);
+        $result = $stmt->execute();
+        $result = $result->fetch();
+        // store id_album for the next query
+        $album =  $result->id_album;
+        // store the results
+        $details->id = $result->id_vinyl;
+        $details->cost = $result->cost;
+        $details->rpm = $result->rpm;
+        $details->inch = $result->inch;
+        $details->type = $result->type;
+        $details->title = $result->title;
+        $details->release_date = $result->release_date;
+        $details->cover_img = $result->cover_img;
+        $details->artist = $result->artist;
+        // prepare second statement
+        $stmt = $this->conn->prepare($tracks);
+        $stmt->bind_params("i", $album);
+        $result = $stmt->execute();
+        // create a list to store all (title, duration) tracks
+        $track_list = [];
+        foreach ($result->fetch_all() as $row) {
+            array_push($track_list, [$row->title, $row->duration]);
+        }
+        // also add tracks to details
+        $details->tracks = $track_list;
+        return json_encode($details);
+    }
 }
 ?>
