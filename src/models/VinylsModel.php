78b0446d752a80f5aa003dfe97a3b886416e7cf9
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-12-08 14:37:12 +0100

    implemented suggested vinyls

--- src/models/VinylsModel.php
+++ src/models/VinylsModel.php
@@ -7,7 +7,7 @@ final class VinylsModel {
     private $db = Database::getInstance();
 
     /**
-     * get a specified number of vinyls from the database.
+     * Gets a specified number of vinyls from the database.
      * @param n the number of vinyls to send
      * @param params a map of values to query the database on:
      * params structure:
@@ -18,7 +18,7 @@ final class VinylsModel {
      * @return json of vinyls data
      */    
     public function getVinyls($n = -1, $params) {
-        $vinyls = array();
+        $vinyls = [];
         $query = "SELECT
             v.id_vinyl,
             v.cost,
@@ -57,7 +57,7 @@ final class VinylsModel {
         } else {
             $result = $this->db->executeResults($query);
         }
-        foreach ($result as $row) {
+        foreach ($result as $row):
             // create an empty object
             $json = new stdClass();
             // extract datas from record
@@ -69,12 +69,12 @@ final class VinylsModel {
             $json->artist = $row->artist;
             // add new vinyl topo vinyls list
             array_push($vinyls, $json);
-        }
+        endforeach;
         return json_encode($vinyls);
     }
 
     /**
-     * gets the details of a single vinyl (vinyl page)
+     * Gets the details of a single vinyl (vinyl page)
      * from a given id.
      * @param id of the vinyl in question
      * @return json containing details on the vinyl
@@ -127,17 +127,17 @@ final class VinylsModel {
         $result = $this->db->executeResults($tracks, "i", $album);
         // create a list to store all (title, duration) tracks
         $track_list = [];
-        foreach ($result as $row) {
+        foreach ($result as $row):
             array_push($track_list, [$row->title, $row->duration]);
-        }
+        endforeach;
         // also add tracks to details
         $details->tracks = $track_list;
         return json_encode($details);
     }
 
     /**
-     * function to be called to get the preview of a vinyl
-     * (cart and checkout pages).
+     * Function to be called to get the preview of a vinyl
+     * (cart, checkout and order pages).
      * @param id of the vinyl to get the preview of
      * @return json containing the information on the vinyl
      */
@@ -172,6 +172,69 @@ final class VinylsModel {
             $preview->artist = $result->artist;
         endif;
         return json_encode($preview);
+    }
+
+    /**
+     * Gets the order previews (user page)
+     * from a given vinyl id.
+     * @param id of the vinyl to get the preview of
+     * @return json with the  preview infos
+     */
+    public function getUserOrderPreview($id) {
+        $preview = new stdClass();
+        $query = "SELECT
+            v.cost,
+            a.title,
+            a.cover_img,
+            FROM 
+            vinyls v
+            JOIN albums a ON v.id_vinyl = a.id_album";
+        // execute query
+        $result = $this->db->executeResults($query);
+        if (!empty($result)):
+            // store results
+            $preview->cost = $result->cost;
+            $preview->title = $result->title;
+            $preview->cover_img = $result->cover_img;
+        endif;
+        return json_encode($preview);
+    }
+
+    public function getSuggested($id) {
+        $suggested = [];
+        $infos = "SELECT
+            v.id_vinyl
+            a.genre,
+            ar.name AS artist
+            FROM
+            vinyls v
+            JOIN albums a ON v.id_album = a.id_album
+            JOIN artists ar ON ar.id_artist = a.id_artist
+            WHERE v.id_vinyl = ?";
+        $vinyls = "SELECT
+            v.id_vinyl,
+            a.cover_img,
+            a.title
+            FROM
+            vinyls v
+            JOIN albums a ON v.id_album = a.id_album
+            JOIN artists ar ON ar.id_artist = a.id_artist
+            WHERE (a.genre = ? OR ar.name = ?)
+            AND v.id_vinyl <> ?
+            LIMIT 6";
+        // execute query
+        $result = $this->db->executeResults($infos, "i", $id);
+        // store infos
+        if (!empty($result)):
+            $result = $this->db->executeResults($vinyls, ["s", "s", "i"], [$result->genre, $result->artist, $id]);
+            foreach($result as $row):
+                $vinyl = new stdClass();
+                $vinyl->cover_img = $row->cover_img;
+                $vinyl->title = $row->title;
+                array_push($suggested, $vinyl);
+            endforeach;
+        endif;
+        return json_encode($suggested);
     }
 }
 ?>
