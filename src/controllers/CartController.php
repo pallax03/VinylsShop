1a5b20998f98ba18def0184169b044fab0923dba
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-12-26 15:48:37 +0100

    fixed search css style

--- src/controllers/CartController.php
+++ src/controllers/CartController.php
@@ -3,6 +3,7 @@ class CartController extends Controller {
 
     private $auth_model = null;
     private $user_model = null;
+    private $vinyls_model = null;
 
     public function __construct() {
         require_once MODELS . 'AuthModel.php';
@@ -10,6 +11,9 @@ class CartController extends Controller {
 
         require_once MODELS . 'UserModel.php';
         $this->user_model = new UserModel();
+
+        require_once MODELS . 'VinylsModel.php';
+        $this->vinyls_model = new VinylsModel();
     }
 
     public function index(Request $request, Response $response) {
