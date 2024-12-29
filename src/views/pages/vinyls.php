da3c81d9c2bb38eafbbc0f06adb97cf060fbad0a
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-12-29 19:27:28 +0100

    implementing vinyls view

--- src/views/pages/vinyls.php
+++ src/views/pages/vinyls.php
@@ -1,3 +1,14 @@
 <section class="vinyl-details">
     <img class="vinyl-details" title=<?php echo $data["vinyl"]["details"]["title"] ?> src="/resources/img/albums/<?php echo $data["vinyl"]["details"]["cover"]?>"/>
-</section>    
+    <p class="info">
+        <?php
+            $details = $data["vinyl"]["details"];
+            echo $details["rpm"] . " - " . $details["inch"]
+                . " - " . $details["type"];
+        ?>
+    </p>
+    <a class="add-cart">Add to cart - cost €<?php echo $data["vinyl"]["details"]["cost"] ?></a>
+</section>
+<section class="tracklist">
+    <h1 class="vinyl-title"><?php echo $data["vinyl"]["details"]["title"] ?></h1>
+</section>
